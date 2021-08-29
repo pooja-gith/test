c7b4c34cb3a5aaab65e5fb68c9b63d702f3d89b5
pooja-gith <poojasirmour@gmail.com>
2021-08-29 20:12:26 +0200

    commit on github

---
+++ my-portfolio/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addclass("sticky");
+    } else {
+      $(".navbar").removeclass("sticky");
+    }
+  });
+  //toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
